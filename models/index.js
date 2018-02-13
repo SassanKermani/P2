76e5388beb386930c85ac3853c1c4089ac7449df
SassanKermani <SassanKermani2@gmail.com>
2018-02-12 18:07:46 -0700

    asdfas

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/P2");
+//mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/P2");
 
 module.exports.chat = require('./chat.js');
 module.exports.User = require('./user.js');
